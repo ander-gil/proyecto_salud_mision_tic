2cbcf8832d2bd265916d501bdae54d648fc71c48
Enriquecarvajalnunez <63138636+Enriquecarvajalnunez@users.noreply.github.com>
2022-09-19 19:39:32 -0500

    url aldair
    url aldair

--- gestionTransaccion/urls.py
+++ gestionTransaccion/urls.py
@@ -4,7 +4,8 @@ from gestionTransaccion.views import EmpresaView, PersonasView, UsuarioView, Tra
 urlpatterns = [
     path('Empresas/',EmpresaView.as_view(),name = 'Listar'),
     path('Empresas/<str:id_empresa>', EmpresaView.as_view(), name='buscar'),
-    path('Persona/',PersonasView.as_view(),name = 'Listar'), 
+    path('Persona/',PersonasView.as_view(),name = 'Listar'),
+    path('Persona/<str:id_persona>',PersonasView.as_view(),name = 'buscar'),  
     path('Usuario/',UsuarioView.as_view(),name = 'Listar'),
     path('Transacciones/<str:id_empresa>', TransaccionesView.as_view(), name='buscar'),                       
     path('Transacciones/',TransaccionesView.as_view(),name = 'Listar'),                       
